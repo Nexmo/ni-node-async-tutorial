9aed45203e5c53c2ccec6efd91102ac6c454f4d5
Mark Lewin <mark.lewin@vonage.com>
2018-10-22 16:21:43 +0100

    Apply minor edits from review of associated tutorial

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000);
 app.use(bodyParser.json());
 
 const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
 const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
 const WEBHOOK_URL = process.env.WEBHOOK_URL;
 
-var Nexmo = require('nexmo');
-var nexmo = new Nexmo({
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
     apiKey: NEXMO_API_KEY,
     apiSecret: NEXMO_API_SECRET
 });
